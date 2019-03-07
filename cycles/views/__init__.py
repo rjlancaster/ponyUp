4b21fc22106eaa6dfdf6601d69e5613570eeed98
rjlancaster <rjlancaster@gmail.com>
2019-03-07 14:20:15 -0600

    edit recurring bill displays on edit bill form

--- cycles/views/__init__.py
+++ cycles/views/__init__.py
@@ -1,5 +1,5 @@
 from .auth import index, register, login_user, user_logout
-from .cyclelist import cyclelist, deleteCycle, cycleDetail
+from .cyclelist import cyclelist, deleteCycle, cycleDetail, editBill, editBillForm
 from .tenantlist import tenantlist, deleteTenant, editTenant, editTenantForm, addTenant, addTenantForm
 
 
--- cycles/views/cyclelist.py
+++ cycles/views/cyclelist.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.db.models import Count
+from django.db.models import Count, Sum
 from ..models import Cycle, Tenant, tenantCycle, Bills
 
 def cyclelist(request):
@@ -12,15 +12,16 @@ def cyclelist(request):
 def cycleDetail(request, cycle_id):
     managerId = request.user.id
     recurringBills = Bills.objects.filter(recurring=1, cycle=cycle_id)
-    recurringBillsDue = Bills.objects.filter(recurring=1, cycle=cycle_id).count()
-    # print(recurringBillsDue)
+    recurringBillsDue = Bills.objects.filter(recurring=1, cycle=cycle_id).aggregate(Sum('amount'))
+    print(recurringBillsDue)
     oneTimeBills = Bills.objects.filter(recurring=0, cycle=cycle_id)
+    oneTimeBillsDue = Bills.objects.filter(recurring=0, cycle=cycle_id).aggregate(Sum('amount'))
     cycle = get_object_or_404(Cycle, pk=cycle_id)
     tenants = Tenant.objects.filter(deletedOn=None, pk=managerId)
     currentTenants = Tenant.objects.filter(cycle=cycle_id)
     numberOfTenants = Tenant.objects.filter(cycle=cycle_id).count()
-    print(numberOfTenants)
-    context = {'recurringBills' : recurringBills, 'recurringBillsDue': recurringBillsDue, 'oneTimeBills' : oneTimeBills, 'cycle': cycle, 'tenants': tenants, 'currentTenants': currentTenants}
+    # print(numberOfTenants)
+    context = {'recurringBills' : recurringBills, 'recurringBillsDue': recurringBillsDue, 'oneTimeBillsDue': oneTimeBillsDue, 'oneTimeBills' : oneTimeBills, 'cycle': cycle, 'tenants': tenants, 'currentTenants': currentTenants}
     return render(request, 'cycles/cycleDetail.html', context)
 
 def deleteCycle(request, cycle_id):
@@ -43,3 +44,24 @@ def deleteCycle(request, cycle_id):
     cycle = Cycle.objects.filter(pk=cycle_id)
     cycle.delete()
     return HttpResponseRedirect(reverse('cycles:index'))
+
+def editBillForm(request, bill_id):
+    billRow = get_object_or_404(Bills, pk=bill_id)
+    context = {'billRow': billRow}
+    return render(request, 'cycles/editBillForm.html', context)
+
+def editBill(request, bill_id):
+    """R Lancaster[This method is executed when the user saves the updated user settings on the user settings update form page]
+
+    Arguments:
+        request
+
+    Returns:
+        User is redirected to main User Settings page.
+    """
+    bill = Bills.objects.get(pk=bill_id)
+    bill.name = request.POST['name']
+    bill.amount = request.POST['amount']
+    # print(tenant.name, tenant.income)
+    bill.save()
+    return HttpResponseRedirect(reverse('cycles:cycleDetail', bill.cycle))
